58d27c4c9bf8cffa200f4eeb8894a26ed6c5e749
Oster.Huang <oster.huang@gramltd.com>
2023-01-10 18:01:18 +0800

    Initial Commit

---
+++ ipass/ipassApp.swift
@@ -0,0 +1,17 @@
+//
+//  ipassApp.swift
+//  ipass
+//
+//  Created by Oster Huang on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ipassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
